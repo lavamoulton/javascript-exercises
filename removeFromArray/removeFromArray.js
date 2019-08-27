d2e45961741689fb17650bfc078e0d1681381437
lavamoulton <danielzmoulton@gmail.com>
2019-08-27 13:17:27 -0400

    removeFromArray solution

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(list) {
+	
+	for (var i=0; i<arguments.length; i++) {
+		
+		var current = arguments[i];
+		for (var j=0; j<list.length; j++) {
 
+			if (current == list[j]) {
+				list.splice(j, 1);
+			}
+		}
+	}
+
+	return list;
 }
 
 module.exports = removeFromArray
