eb9ef85b6ae3a5a1149830571fbc7f2e247cc308
lavamoulton <danielzmoulton@gmail.com>
2019-08-27 11:50:24 -0400

    reverseString solution

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(word) {
+	length = word.length;
+	result = "";
 
+	for (var i=0; i<length; i++) {
+		result = result + word.charAt(length - 1 - i);	
+	}
+
+	return result;
 }
 
 module.exports = reverseString
